8d1cca16a88c75195b08630ded41a536b2ec9719
Asma Skhiri <asma.skhiri.267@gmail.com>
2023-08-15 13:20:51 +0100

    céation d'un fichier CSS

--- musicex/listings/models.py
+++ musicex/listings/models.py
@@ -4,6 +4,7 @@ class Band(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
 
+
 class Listing(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
